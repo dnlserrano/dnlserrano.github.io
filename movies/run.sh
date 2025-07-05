bc7df445e03f7c819af14c7cde02dfe354dd2175
Daniel Serrano <daniel.serrano@onfido.com>
2025-07-05 13:57:10 +0100

    Update movies page

--- movies/run.sh
+++ movies/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-wget https://datasets.imdbws.com/title.basics.tsv.gz
-gzip -d title.basics.tsv.gz
-
-wget https://datasets.imdbws.com/title.akas.tsv.gz
-gzip -d title.akas.tsv.gz | gzip -d
+for file in title.basics.tsv.gz title.akas.tsv.gz title.crew.tsv.gz title.ratings.tsv.gz name.basics.tsv.gz; do
+    wget https://datasets.imdbws.com/$file
+    gzip -d $file
+done
 
 pip install duckdb
-pip install IMDbPY
 pip install jinja2
+pip install movieposters
 
 python generate.py > ../_includes/movies.html
